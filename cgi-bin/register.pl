05f2fe7b695c8ac34e22db37abbc1fcb08afe87d
Romina <rcamargoh@unsa.edu.pe>
2024-12-07 16:23:49 -0500

    añadi register.pl

---
+++ cgi-bin/register.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use DBI;
+use CGI;
+
+# Configuración de la base de datos
+my $dsn = "DBI:mysql:database=tu_base_de_datos;host=localhost";
+my $usuario = "root";
+my $clave = "tu_contraseña";
+my $dbh = DBI->connect($dsn, $usuario, $clave, { RaiseError => 1, AutoCommit => 1 });
+
+# Obtener datos del formulario
+my $cgi = CGI->new;
+my $userName = $cgi->param('userName');
+my $password = $cgi->param('password');
+my $firstName = $cgi->param('firstName');
+my $lastName = $cgi->param('lastName');
+
+# Insertar nuevo usuario en la base de datos
+my $sql = "INSERT INTO users (userName, password, firstName, lastName) VALUES (?, ?, ?, ?)";
+my $sth = $dbh->prepare($sql);
+$sth->execute($userName, $password, $firstName, $lastName);
+
+print $cgi->header(-type => 'text/html');
+print "<p>Usuario registrado exitosamente</p>";
