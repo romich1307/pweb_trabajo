aeeedc1d889aa1b34cd78ca83427330d18797788
Tania Luz Ayque Puraca <tayque@unsa.edu.pe>
2024-12-13 12:25:30 -0500

    añadido estilo para textarea para que sea mas como de usar

--- js/myScript.js
+++ js/myScript.js
@@ -239,12 +239,12 @@ function showList(xml) {
 function showNew(){
 
     let showNew = `<p>Titulo</p>
-    <input id='titulo' name ='titulo' type='text'><br>
+    <input id='titulo' name='titulo' type='text'><br>
     <p>Contenido-markdown</p>
-    <textarea style = "width: 100%;" type="text" id ="cuerpo" name="cuerpo"></textarea><br>
+    <textarea id="cuerpo" name="cuerpo"></textarea><br>
     <button onclick='doNew()'>Enviar</button>
     <button onclick='doList()'>Cancelar</button>`;
-  
+    
     document.getElementById("main").innerHTML = showNew;
 
 }
