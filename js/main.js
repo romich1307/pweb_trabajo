0370902b1a5cbed722984726401acde0878f0412
Tania Luz Ayque Puraca <tayque@unsa.edu.pe>
2024-12-12 22:21:17 -0500

    Agrego script de bienvenida en main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Variables locales 
+var userFullName = '';
+var userKey = '';
+
+//Bienvenida a la pagina mostrando una descripcion del proyecto
+window.addEventListener('load', showWelcome);
+function showWelcome(){
+    let html = `<h2>Bienvenido ${userFullName}</h2>
+    <p>Este sistema fue desarrollado por alumnos del primer año de la Escuela Profesional de Ingeniería de Sistemas, de la Universidad Nacional de San Agustín de Arequipa</p>
+    <p>El sistema fué desarrollado usando estas tecnologías:</p>
+    <ul>
+        <li>HTML y CSS</li>
+        <li>Perl para el backend</li>
+        <li>MariaDB para la base de datos</li>
+        <li>Javascript para el frontend</li>
+        <li>Las páginas se escriben en lenguaje Markdown</li>
+        <li>Se usaron expresiones regulares para el procesamiento del lenguaje Markdown</li>
+        <li>La comunicación entre el cliente y el servidor se hizo usando XML de manera asíncrona</li>
+    </ul>`;
+    document.getElementById('main').innerHTML = html;
+}
+
+
