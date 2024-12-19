6c3270abcd88df0a31649e81c2e550a25e86b781
Tania Luz Ayque Puraca <tayque@unsa.edu.pe>
2024-12-19 15:21:14 -0500

    Cambios en el view.pl

--- cgi-bin/view.pl
+++ cgi-bin/view.pl
@@ -1,140 +1,110 @@
 #!/usr/bin/perl
+use DBI;
+use CGI;
 use strict;
 use warnings;
-use CGI;
-use CGI::Carp qw(fatalsToBrowser);
-use CGI::Session;
-use DBI;
 
-my $cgi = CGI->new;
-my $session = CGI::Session->new("driver:File", $cgi, {Directory=>'/tmp'});
+my $q = CGI->new;
+my $owner = $q->param('usuario');
+my $titulo = $q->param('titulo');
 
-my $owner = $session->param('userName');
-if (!$owner) {
-    print $cgi->header('application/json', '401 Unauthorized');
-    print '{"error": "No autorizado"}';
-    exit;
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=db";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+# Obtener el ID del usuario
+my $sth = $dbh->prepare("SELECT id FROM Users WHERE username=?");
+$sth->execute($owner);
+my ($owner_id) = $sth->fetchrow_array;
+$sth->finish;
+
+if ($owner_id) {
+    $sth = $dbh->prepare("SELECT markdown FROM Articles WHERE owner=? AND title=?");
+    $sth->execute($owner_id, $titulo);
+    my @texto = $sth->fetchrow_array;
+    $sth->finish;
+
+    print $q->header('text/XML');
+    print "<?xml version='1.0' encoding='utf-8'?>\n";
+
+    if (@texto && defined $texto[0]) {
+        my @lineas = split "\n", $texto[0];
+        print "<page>\n";
+        for my $linea (@lineas) {
+            print matchLine($linea);
+        }
+        print "</page>\n";
+    } else {
+        print "<page><error>Article not found</error></page>\n";
+    }
+} else {
+    print $q->header('text/XML');
+    print "<?xml version='1.0' encoding='utf-8'?>\n";
+    print "<error>Usuario no encontrado</error>\n";
 }
 
-my $title = $cgi->param('title');
-print $cgi->header('application/json;charset=UTF-8');
-
-# Conexión a la base de datos
-my $usuario = 'alumno';
-my $clave = 'pweb1';
-my $dsn = "DBI:MariaDB:database=pweb1;host=db";
-my $dbh = DBI->connect($dsn, $usuario, $clave) or die("No se pudo conectar a la base de datos!");
-
-# Consultar el contenido del artículo
-my $sth = $dbh->prepare("SELECT markdown FROM Articles WHERE owner=? AND title=?");
-$sth->execute($owner, $title);
-my ($markdown) = $sth->fetchrow_array;
-
-$sth->finish;
 $dbh->disconnect;
 
-if ($markdown) {
-    my $html_content = generarCuerpo($markdown);
-    print '{"content": ' . json_encode($html_content) . '}';
-} else {
-    print '{"error": "Artículo no encontrado"}';
-}
-
-# Subrutina para procesar Markdown y generar HTML
-sub generarCuerpo {
-    my $lineas = shift;
-    my $texto = "";
-
-    # Convertir el Markdown en líneas para procesarlo
-    my @lineas_split = split "\n", $lineas;
-    my $num_lineas = @lineas_split;
-
-    for (my $i = 0; $i < $num_lineas; $i++) {
-        my $linea_convertida = "";
-
-        # Detectar bloques de código (Markdown: ```)
-        if ($lineas_split[$i] =~ /^(```)/) {
-            $i++;
-            my $codigo_inicio = "<pre><code>\n";
-            my $codigo_fin = "</code></pre><br>\n";
-            my $codigo_texto = "";
-            # Procesar hasta encontrar el cierre del bloque de código
-            for ($i; !($lineas_split[$i] =~ /^(```)/) && $i < $num_lineas; $i++) {
-                $codigo_texto .= "   $lineas_split[$i]\n";
-            }
-            $texto .= "$codigo_inicio$codigo_texto$codigo_fin";
-        } else {
-            $linea_convertida = procesarLinea($lineas_split[$i]);
-        }
-        $texto .= "$linea_convertida";
+sub matchLine {
+    my $linea = $_[0];
+    
+    return "" if $linea =~ /^\s*$/;  # Skip empty lines
+    
+    # Headers
+    if ($linea =~ /^(\#{6})\s+(.*)/) {
+        return "<h6>$2</h6>\n";
     }
-
-    return $texto;
-}
-
-# Subrutina para procesar cada línea de Markdown
-sub procesarLinea {
-    my $linea = shift;
-
-    # Descartar las líneas en blanco
-    return "" if ($linea =~ /^\s*$/);
-
-    # Convertir énfasis (_texto_)
-    while ($linea =~ /(.*)(\_)(.*)(\_)(.*)/) {
-        $linea = "$1<em>$3</em>$5";
+    elsif ($linea =~ /^(\#{5})\s+(.*)/) {
+        return "<h5>$2</h5>\n";
     }
-
-    # Convertir enlaces [texto](URL)
-    while ($linea =~ /(.*)(
-
-\[)(.*)(\]
-
-)(.*)(\()(.*)(\))(.*)/) {
-        $linea = "$1<a href='$7'>$3</a>$9";
+    elsif ($linea =~ /^(\#{4})\s+(.*)/) {
+        return "<h4>$2</h4>\n";
     }
-
-    # Convertir negrita y cursiva (**texto** y ***texto***)
+    elsif ($linea =~ /^(\#{3})\s+(.*)/) {
+        return "<h3>$2</h3>\n";
+    }
+    elsif ($linea =~ /^(\#{2})\s+(.*)/) {
+        return "<h2>$2</h2>\n";
+    }
+    elsif ($linea =~ /^(\#{1})\s+(.*)/) {
+        return "<h1>$2</h1>\n";
+    }
+    
+    # Format the line
+    # Bold + Italic
     while ($linea =~ /(.*)(\*\*\*)(.*)(\*\*\*)(.*)/) {
         $linea = "$1<strong><em>$3</em></strong>$5";
     }
-
+    # Bold
     while ($linea =~ /(.*)(\*\*)(.*)(\*\*)(.*)/) {
         $linea = "$1<strong>$3</strong>$5";
     }
-
-    # Convertir cursiva (*texto*)
+    # Italic
     while ($linea =~ /(.*)(\*)(.*)(\*)(.*)/) {
         $linea = "$1<em>$3</em>$5";
     }
-
-    # Convertir tachado (~~texto~~)
-    while ($linea =~ /(.*)(\~\~)(.*)(\~\~)(.*)/) {
+    # Italic with underscore
+    while ($linea =~ /(.*)(\_)(.*)(\_)(.*)/){
+        $linea = "$1<em>$3</em>$5";
+    }
+    # Links
+    while ($linea =~ /(.*)(\[)(.*)(\])(\()(.*)(\))(.*)/) {
+        $linea = "$1<a href='$6'>$3</a>$8";
+    }
+    # Strikethrough
+    while ($linea =~ /(.*)(\~\~)(.*)(\~\~)(.*)/){
         $linea = "$1<del>$3</del>$5";
     }
-
-    # Convertir encabezados (# texto, ## texto, etc.)
-    if ($linea =~ /^(\#)([^#\S].*)/) {
-        return "<h1>$2</h1>\n";
-    } elsif ($linea =~ /^(\#\#)([^#\S].*)/) {
-        return "<h2>$2</h2>\n";
-    } elsif ($linea =~ /^(\#\#\#)([^#\S].*)/) {
-        return "<h3>$2</h3>\n";
-    } elsif ($linea =~ /^(\#\#\#\#)([^#\S].*)/) {
-        return "<h4>$2</h4>\n";
-    } elsif ($linea =~ /^(\#\#\#\#\#)([^#\S].*)/) {
-        return "<h5>$2</h5>\n";
-    } elsif ($linea =~ /^(\#\#\#\#\#\#)([^\S].*)/) {
-        return "<h6>$2</h6>\n";
-    } else {
-        return "<p>$linea</p>\n";
+    # Lists
+    if ($linea =~ /^\*\s+(.*)/) {
+        return "<li>$1</li>\n";
     }
-}
-
-sub json_encode {
-    my $string = shift;
-    $string =~ s/"/\\"/g;
-    $string =~ s/\n/\\n/g;
-    $string =~ s/\r/\\r/g;
-    $string =~ s/\t/\\t/g;
-    return '"' . $string . '"';
+    # Code blocks
+    elsif ($linea =~ /^```(.*)/) {
+        return "<pre><code>$1</code></pre>\n";
+    }
+    
+    # Regular paragraph
+    return "<p>$linea</p>\n";
 }
