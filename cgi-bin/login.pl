6cd57a88ade246531ec53481db23bfa36b8d2723
Tania Luz Ayque Puraca <tayque@unsa.edu.pe>
2024-12-19 16:42:58 -0500

    Se modifica el login.pl

--- cgi-bin/login.pl
+++ cgi-bin/login.pl
@@ -1,53 +1,34 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+use DBI;
+use CGI;
 use strict;
 use warnings;
-use CGI;
-use DBI;
 
-# Crear una instancia de CGI para obtener los parámetros del formulario
-my $query = CGI->new;
+my $q = CGI->new;
+my $username = $q->param('usuario');
+my $password = $q->param('password');
 
-# Obtener los datos del formulario
-my $username = $query->param('username');
-my $password = $query->param('password');
+my $user = 'alumno';
+my $dbpassword = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=db";
+my $dbh = DBI->connect($dsn, $user, $dbpassword) or die("No se pudo conectar!");
 
-# Verificar si los campos están completos
-if (!$username || !$password) {
-    print $query->header('text/html');
-    print "<html><body><script>alert('Por favor ingrese ambos campos: usuario y contraseña.'); window.location.href = '../login.html';</script></body></html>";
-    exit;
+my $sth = $dbh->prepare("SELECT id, firstName, lastName FROM Users WHERE username=? AND password=?");
+$sth->execute($username, $password);
+
+print $q->header('text/XML');
+print "<?xml version='1.0' encoding='utf-8'?>\n";
+
+my @row = $sth->fetchrow_array;
+if (@row) {
+    print "<user>\n";
+    print "<owner>$row[0]</owner>\n";
+    print "<firstName>$row[1]</firstName>\n";
+    print "<lastName>$row[2]</lastName>\n";
+    print "</user>\n";
+} else {
+    print "<user></user>\n";
 }
 
-# Configuración de la base de datos
-my $dsn = "DBI:mysql:database=pweb1;host=db";  # Asegúrate de que el 'host' esté configurado correctamente
-my $db_user = "alumno";  # Usuario de la base de datos
-my $db_pass = "pweb1";   # Contraseña de la base de datos
-
-# Conectar a la base de datos
-my $dbh = DBI->connect($dsn, $db_user, $db_pass, { RaiseError => 1, AutoCommit => 1 })
-    or die "No se pudo conectar a la base de datos: $DBI::errstr";
-
-# Preparar la consulta para obtener el usuario y su contraseña
-my $sth = $dbh->prepare("SELECT * FROM Users WHERE username = ? LIMIT 1");
-$sth->execute($username);
-
-# Verificar si el usuario existe
-my $user = $sth->fetchrow_hashref;
-if (!$user) {
-    # Usuario no encontrado
-    print $query->header('text/html');
-    print "<html><body><script>alert('Usuario no encontrado.'); window.location.href = '../login.html';</script></body></html>";
-    exit;
-}
-
-# Verificar si la contraseña es correcta (aquí deberías comparar la contraseña de manera segura)
-if ($user->{password} ne $password) {
-    # Contraseña incorrecta
-    print $query->header('text/html');
-    print "<html><body><script>alert('Contraseña incorrecta.'); window.location.href = '../login.html';</script></body></html>";
-    exit;
-}
-
-# Si todo es correcto, redirigir al index
-print $query->header('text/html');
-print "<html><body><script>window.location.href = '../index.html';</script></body></html>";
+$sth->finish;
+$dbh->disconnect;
