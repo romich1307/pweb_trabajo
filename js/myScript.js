b7635117a705332f744a808d2f830ae75b8522b9
Tania Luz Ayque Puraca <tayque@unsa.edu.pe>
2024-12-19 17:26:02 -0500

    Añado función para crear cuenta de usuario y manejar registro en myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -59,3 +59,66 @@ function showLoggedIn(){
     showWelcome();
     showMenuUserLogged();
 }
+/**
+ * Esta función crea el formulario para el registro de nuevos usuarios
+ * el fomulario se mostrará en tag div con id main.
+ * La acción al presionar el bontón de Registrar será invocar a la 
+ * función doCreateAccount
+ * */
+function showCreateAccount(){
+
+    const mainTag = document.getElementById("main");
+
+    var html = `
+    <label>Nombres: </label> 
+    <input type='text' id='Nombre' name='Nombre' required><br>
+    <label>Apellidos: </label> 
+    <input type='text' id='Apellido' name='Apellido' required><br>
+    <p>Usuario</p> 
+    <input type='text' id='usuario' name='usuario' required><br>
+    <p>Contraseña</p> 
+    <input type='password' id='password' name='password' required><br>
+    <button style = 'margin-top: 10px' onclick='doCreateAccount()'>Crear cuenta</button>`;
+
+    mainTag.innerHTML = html;
+
+
+}
+
+/* Esta función extraerá los datos ingresados en el formulario de
+ * registro de nuevos usuarios e invocará al CGI register.pl
+ * la respuesta de este CGI será procesada por loginResponse.
+ */
+function doCreateAccount(){
+    
+    // nombre y apellido pueden escribir letras separadas y no habra problema
+    const Nombre = encodeURIComponent(document.getElementById("Nombre").value).replace(/%20/g,"+");
+    const Apellido = encodeURIComponent(document.getElementById("Apellido").value).replace(/%20/g,"+");
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+
+    if (usuario == "" || password == "" || Nombre == "" || Apellido == "") {
+        document.getElementById("Nombre").value = "";
+        document.getElementById("Apellido").value = "";
+        document.getElementById("usuario").value = "";
+        document.getElementById("password").value = "";
+    }
+    else {
+
+      let url = `${SERVER_URL}/cgi-bin/register.pl?usuario=${usuario}&password=${password}&Nombre=${Nombre}&Apellido=${Apellido}`
+        console.log(url);
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("GET", url, true);
+        xhr.send();
+        // registramos cuenta y la logeamos automaticamente
+        xhr.onload = function () {
+            loginResponse(xhr.responseXML);
+        };
+
+    }
+
+
+}
