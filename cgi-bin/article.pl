5fbf676fbf51098250da06255e04c5dc3d85c1fb
Tania Luz Ayque Puraca <tayque@unsa.edu.pe>
2024-12-19 13:26:17 -0500

    Cambios en article.pl donde búsqueda del ID del usuario antes de recuperar el "artículo".

--- cgi-bin/article.pl
+++ cgi-bin/article.pl
@@ -9,26 +9,49 @@ my $q = CGI->new;
 my $owner = $q->param('owner');
 my $title = $q->param('title');
 
-print $q->header('text/xml');
-print "<?xml version='1.0' encoding='utf-8'?>\n";
 
 my $user = 'alumno';
 my $password = 'pweb1';
 my $dsn = "DBI:MariaDB:database=pweb1;host=db";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
-      
-my $sth = $dbh->prepare("SELECT * FROM Articles WHERE title=? AND owner=?");
-$sth->execute($title,$owner);
-my @row = $sth->fetchrow_array;
+  #Agrego que primero busque el id del usuario    
+my $sth = $dbh->prepare("SELECT id FROM Users WHERE username=?");
+$sth->execute($owner);
+my ($owner_id) = $sth->fetchrow_array;
 $sth->finish;
-$dbh->disconnect;
 
-if(@row){
-  print "<article>\n";
-  print "<owner>$owner</owner>\n";
-  print "<title>$title</title>\n";
-  print "<text>$row[2]</text>\n";
-  print "</article>\n";
-}else{  
-  print "<article>\n</article>\n";
-}  
+if ($owner_id) {
+    $sth = $dbh->prepare("SELECT markdown FROM Articles WHERE owner=? AND title=?");
+    $sth->execute($owner_id, $titulo);
+    my @row = $sth->fetchrow_array;
+
+    print $q->header('text/xml');
+    print "<?xml version='1.0' encoding='utf-8'?>\n";
+      if (!(@row == 0)){
+          # si existe una pagina con el usuario y titulo dado.
+          $sth->finish;
+
+          print "<article>";
+          print "<owner>$owner</owner>";
+          print "<title>$titulo</title>";
+          print "<text>@row</text>";
+          print "</article>";
+      }
+      else {
+          print "<article>";
+          print "<owner></owner>";
+          print "<title></title>";
+          print "<text></text>";
+          print "</article>";
+      }
+} else {
+    print $q->header('text/XML');
+    print "<?xml version='1.0' encoding='utf-8'?>\n";
+    print "<article>";
+    print "<owner></owner>";
+    print "<title></title>";
+    print "<text></text>";
+    print "</article>";
+}
+
+$dbh->disconnect;
